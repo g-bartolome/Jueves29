b934cd118f1abfb5de788816228f6ddcfbdd3827
g-bartolome <132186400+g-bartolome@users.noreply.github.com>
2025-05-27 20:09:40 +0200

    Añadida la despedida del mundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -12,6 +12,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("Adios mundo");
     }
     
 }
